872625febfa06d42a011d0204022e0d1672893ed
Jonathan <jw4107@nyu.edu>
2021-12-24 15:30:50 -0500

    Add search functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from "react"
-import { useFetch } from 'react-fetch-hook'
-import { useState } from 'react-fetch-hook'
-import { useEffect } from 'react-fetch-hook'
+import useFetch from 'react-fetch-hook'
+import { useEffect, useState } from 'react'
 import ContactCards from './ContactCards'
 
 const App = () => {
+  const url = 'https://randomuser.me/api/'
+  const { isLoading, data, error } = useFetch(url+'?results=200')
+  const [contactList, setContactList] = useState()
+  const [filterQuery, setFilterQuery] = useState()
+
+  useEffect(() => {
+      if (!filterQuery) {
+        setContactList(data?.results?.slice(0, 20))
+      } else {
+        const queryString = filterQuery.toLowerCase()
+        const filteredData = data?.results?.filter(contact => {
+          const fullName = `${contact.name.first} ${contact.name.last}`
+
+          // if it's just one letter, return all names that start with it
+          if (queryString.length === 1) {
+            const firstLetter = fullName.charAt(0).toLowerCase()
+            return firstLetter === queryString
+          }
+          else {
+            return fullName.toLowerCase().includes(queryString)
+          }
+        })
+        setContactList(filteredData)
+      }
+    }, [data, filterQuery])
+
   return (
     <div className="bg-gray-100">
       <section>
@@ -12,12 +36,16 @@ const App = () => {
           <input
             type="text"
             className="ml-20 mt-6 rounded-md p-2"
+            onChange={event => setFilterQuery(event.target.value)}
             placeholder="type here to filter..."
           />
         </form>
       </section>
       <section className="p-20 grid sm:grid-cols-2 md:grid-cols-4 gap-6">
-        <ContactCards />
+        {contactList?.length < 1 && (
+          <h1>No data matches your search</h1>
+        )}
+        <ContactCards contactList={contactList}/>
       </section>
     </div>
   );
--- src/ContactCards.jsx
+++ src/ContactCards.jsx
@@ -1,198 +1,27 @@
 const ContactCards = ({ contactList }) => {
-  contactList && console.log(contactList)
   return (
     <>
-      <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
-        <img
-          alt="user"
-          className="w-32 h-32 rounded-full mx-auto"
-          src="https://thumbs.dreamstime.com/b/delivery-concept-portrait-happy-african-american-man-holding-box-package-showing-thumps-up-isolated-grey-studio-112456787.jpg"
-        />
-        <figcaption className="text-center mt-5">
-          <p className="text-gray-700 font-semibold text-xl mb-2">
-            Billy Joe
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">email:</span> b.joe@email.com
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">phone:</span> (800) 765-34-90
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">city:</span> Texas
-          </p>
-        </figcaption>
-      </figure>
-      <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
-        <img
-          alt="user"
-          className="w-32 h-32 rounded-full mx-auto"
-          src="https://thumbs.dreamstime.com/b/delivery-concept-portrait-happy-african-american-man-holding-box-package-showing-thumps-up-isolated-grey-studio-112456787.jpg"
-        />
-        <figcaption className="text-center mt-5">
-          <p className="text-gray-700 font-semibold text-xl mb-2">
-            Billy Joe
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">email:</span> b.joe@email.com
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">phone:</span> (800) 765-34-90
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">city:</span> Texas
-          </p>
-        </figcaption>
-      </figure>
-      <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
-        <img
-          alt="user"
-          className="w-32 h-32 rounded-full mx-auto"
-          src="https://thumbs.dreamstime.com/b/delivery-concept-portrait-happy-african-american-man-holding-box-package-showing-thumps-up-isolated-grey-studio-112456787.jpg"
-        />
-        <figcaption className="text-center mt-5">
-          <p className="text-gray-700 font-semibold text-xl mb-2">
-            Billy Joe
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">email:</span> b.joe@email.com
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">phone:</span> (800) 765-34-90
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">city:</span> Texas
-          </p>
-        </figcaption>
-      </figure>
-      <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
-        <img
-          alt="user"
-          className="w-32 h-32 rounded-full mx-auto"
-          src="https://thumbs.dreamstime.com/b/delivery-concept-portrait-happy-african-american-man-holding-box-package-showing-thumps-up-isolated-grey-studio-112456787.jpg"
-        />
-        <figcaption className="text-center mt-5">
-          <p className="text-gray-700 font-semibold text-xl mb-2">
-            Billy Joe
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">email:</span> b.joe@email.com
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">phone:</span> (800) 765-34-90
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">city:</span> Texas
-          </p>
-        </figcaption>
-      </figure>
-      <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
-        <img
-          alt="user"
-          className="w-32 h-32 rounded-full mx-auto"
-          src="https://thumbs.dreamstime.com/b/delivery-concept-portrait-happy-african-american-man-holding-box-package-showing-thumps-up-isolated-grey-studio-112456787.jpg"
-        />
-        <figcaption className="text-center mt-5">
-          <p className="text-gray-700 font-semibold text-xl mb-2">
-            Billy Joe
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">email:</span> b.joe@email.com
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">phone:</span> (800) 765-34-90
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">city:</span> Texas
-          </p>
-        </figcaption>
-      </figure>
-      <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
-        <img
-          alt="user"
-          className="w-32 h-32 rounded-full mx-auto"
-          src="https://thumbs.dreamstime.com/b/delivery-concept-portrait-happy-african-american-man-holding-box-package-showing-thumps-up-isolated-grey-studio-112456787.jpg"
-        />
-        <figcaption className="text-center mt-5">
-          <p className="text-gray-700 font-semibold text-xl mb-2">
-            Billy Joe
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">email:</span> b.joe@email.com
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">phone:</span> (800) 765-34-90
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">city:</span> Texas
-          </p>
-        </figcaption>
-      </figure>
-      <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
-        <img
-          alt="user"
-          className="w-32 h-32 rounded-full mx-auto"
-          src="https://thumbs.dreamstime.com/b/delivery-concept-portrait-happy-african-american-man-holding-box-package-showing-thumps-up-isolated-grey-studio-112456787.jpg"
-        />
-        <figcaption className="text-center mt-5">
-          <p className="text-gray-700 font-semibold text-xl mb-2">
-            Billy Joe
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">email:</span> b.joe@email.com
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">phone:</span> (800) 765-34-90
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">city:</span> Texas
-          </p>
-        </figcaption>
-      </figure>
-      <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
-        <img
-          alt="user"
-          className="w-32 h-32 rounded-full mx-auto"
-          src="https://thumbs.dreamstime.com/b/delivery-concept-portrait-happy-african-american-man-holding-box-package-showing-thumps-up-isolated-grey-studio-112456787.jpg"
-        />
-        <figcaption className="text-center mt-5">
-          <p className="text-gray-700 font-semibold text-xl mb-2">
-            Billy Joe
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">email:</span> b.joe@email.com
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">phone:</span> (800) 765-34-90
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">city:</span> Texas
-          </p>
-        </figcaption>
-      </figure>
-      <figure className="bg-white h-80 rounded-lg shadow-md pt-7">
-        <img
-          alt="user"
-          className="w-32 h-32 rounded-full mx-auto"
-          src="https://thumbs.dreamstime.com/b/delivery-concept-portrait-happy-african-american-man-holding-box-package-showing-thumps-up-isolated-grey-studio-112456787.jpg"
-        />
-        <figcaption className="text-center mt-5">
-          <p className="text-gray-700 font-semibold text-xl mb-2">
-            Billy Joe
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">email:</span> b.joe@email.com
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">phone:</span> (800) 765-34-90
-          </p>
-          <p className="text-gray-500">
-            <span className="font-medium">city:</span> Texas
-          </p>
-        </figcaption>
-      </figure>
+      {contactList?.map((contact, index) => (
+        <figure className="bg-white text-white h-80 rounded-lg shadow-md" key={index}>
+          <img alt="user" className="w-32 h-32 rounded-full mx-auto mt-7" src={contact.picture.large} />
+          <figcaption className="text-center mt-5">
+            <p className="text-gray-700 font-semibold text-xl mb-2">
+              {contact.name.first} {contact.name.last}
+            </p>
+            <p className="text-gray-500">
+              <span className="font-medium">email: </span>{contact.email}
+            </p>
+            <p className="text-gray-500">
+              <span className="font-medium">phone: </span>{contact.cell}
+            </p>
+            <p className="text-gray-500">
+              <span className="font-medium">city: </span>{contact.location.city}
+            </p>
+          </figcaption>
+        </figure>
+      ))}
     </>
   )
-};
+}
 
-export default ContactCards;
+export default ContactCards
